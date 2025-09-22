0024edba022ae637a054e4adb824091f771a996d
kelvinchiluwe <kelvincchiluwe@gmail.com>
2025-09-22 10:42:06 +0200

    Update embeddings.py

--- KelvinProjects/chicken_chat_bot/chicks/embeddings/embeddings.py
+++ KelvinProjects/chicken_chat_bot/chicks/embeddings/embeddings.py
@@ -1,13 +1,4 @@
-# from langchain_openai import OpenAIEmbeddings
-# embeddings=OpenAIEmbeddings(
-#     model="embedding-3",
-  
-#     # api_key="dadc4e4304cd4c169424134357a0a8db.C29f5GShQyrOQdVl",
-#     # api_key="39d21d2a55504df58423d1e4f218ec94.OTy73GoacNLqHGuS",
-#     # api_key=("e0c7cd5ec96c44b8a88873efda1b61ac.fyJdvSoyTAHFNLkM"),
-#     api_key="5dd8688cd8b04521ad49546bf0ef37fb.WxHGfeb5a9Iy1mv8",
-#     base_url="https://open.bigmodel.cn/api/paas/v4"
-# )
+
 
 from langchain_community.embeddings import HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
